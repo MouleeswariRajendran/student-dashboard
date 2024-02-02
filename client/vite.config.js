3b19917fb282718d8de3cdfb87ce04316750d73d
unknown <rmouleeswari@gmail.com>
2024-02-02 20:59:20 +0530

    src

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'client',
+    outDir: 'src',
     chunkSizeWarningLimit: 1000, // Set your preferred limit
     // ... other configurations
   },
